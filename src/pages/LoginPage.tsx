5faaa6ad3f8921575b3e7c277e197a0d3be8a6e1
Shreejal Maharjan <shreejal27@gmail.com>
2025-07-22 23:32:53 +0545

    created LoginPage

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Alert,
+  Paper,
+} from "@mui/material";
+import api from "../api/axios";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const res = await api.post("/auth/login", { email, password });
+      const token = res.data.token;
+
+      localStorage.setItem("token", token);
+      navigate("/dashboard");
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ padding: 4, marginTop: 10 }}>
+        <Typography variant="h5" align="center" gutterBottom>
+          Login
+        </Typography>
+
+        {error && <Alert severity="error">{error}</Alert>}
+
+        <Box component="form" onSubmit={handleLogin} mt={2}>
+          <TextField
+            fullWidth
+            label="Email"
+            margin="normal"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            margin="normal"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            type="submit"
+            sx={{ marginTop: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
+
+export default LoginPage;
