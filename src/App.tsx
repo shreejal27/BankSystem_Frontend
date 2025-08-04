0440482eaa7b7bf15dcbbaee62a18d870c12216b
Shreejal Maharjan <shreejal27@gmail.com>
2025-08-04 23:58:58 +0545

    cleared the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import TransferPage from "./pages/TransferPage";
-import PrivateRoute from "./router/PrivateRoute";
-import { WithdrawPage } from "./pages/WithdrawPage";
-import Dashboard from "./pages/Dashboard";
-import LandingPage from "./pages/LandingPage";
-import { TransactionHistoryPage } from "./pages/TransactionHistoryPage";
-import { AccountProfilePage } from "./pages/AccountProfilePage";
+import AppRouter from "./router/Router";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/transfer" element={<TransferPage />} />
-        <Route path="/withdraw" element={<WithdrawPage />} />
-        <Route path="/accountprofilepage" element={<AccountProfilePage />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route
-            path="/transactionHistoryPage"
-            element={<TransactionHistoryPage />}
-          />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <AppRouter />;
 }
 
 export default App;
