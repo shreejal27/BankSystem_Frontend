913892113ee8c791a55c6973dd27e394925d66ce
Shreejal Maharjan <shreejal27@gmail.com>
2025-08-13 00:38:50 +0545

    created useLogin and loginUser command

--- src/queries/auth/AuthCommand.ts
+++ src/queries/auth/AuthCommand.ts
@@ -1,4 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { LoginQueryKey } from "./AuthQueryKey";
+import { apiClientBe } from "../../api/Client/apiClientBe";
+
 export const useLogin = () => {
     const queryClient = useQueryClient();
-    
+    return useMutation({
+        mutationFn: (data: TLoginSchema) => loginUser(data),
+        onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: [LoginQueryKey] });
+        },
+        onError(error) {
+           return error;
+    }
+});
+};
+
+async function loginUser(data: TLoginSchema): Promise<TLoginResponse> {
+    const response = await apiClientBe.post<TLoginResponse>("/auth/login", data);
+    return response.data;
 }
