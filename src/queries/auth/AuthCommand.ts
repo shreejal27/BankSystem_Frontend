f476cc5271cd5d4d57bdab5f373fdc0cd8adaae9
Shreejal Maharjan <shreejal27@gmail.com>
2025-08-13 00:45:04 +0545

    made a post function to catch login response

--- src/queries/auth/AuthCommand.ts
+++ src/queries/auth/AuthCommand.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { LoginQueryKey } from "./AuthQueryKey";
 import { apiClientBe } from "../../api/Client/apiClientBe";
 import type { TLoginSchema } from "../../utils/schema/TLoginSchema";
+import type { ILoginResponse } from "../../types/auth/AuthType";
 
 export const useLogin = () => {
     const queryClient = useQueryClient();
@@ -16,7 +17,7 @@ export const useLogin = () => {
 });
 };
 
-async function loginUser(data: TLoginSchema): Promise<TLoginResponse> {
-    const response = await apiClientBe.post<TLoginResponse>("/auth/login", data);
+async function loginUser(data: TLoginSchema): Promise<ILoginResponse> {
+    const response = await apiClientBe.post<ILoginResponse>("api/Auth/login", data);
     return response.data;
 }
