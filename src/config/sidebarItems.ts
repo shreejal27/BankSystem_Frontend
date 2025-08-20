22ca968e826a5f524a70ae09f06c861c4481d334
Shreejal Maharjan <shreejal27@gmail.com>
2025-08-20 23:24:40 +0545

    made a sidebar config items

---
+++ src/config/sidebarItems.ts
@@ -0,0 +1,18 @@
+export const sidebarItemsUser = [
+  { name: "Dashboard", path: "/dashboard" },
+  { name: "Accounts", path: "/accounts" },
+  { name: "Deposit", path: "/deposit" },
+  { name: "Withdraw", path: "/withdraw" },
+  { name: "Transfer", path: "/transfer" },
+  { name: "Transactions", path: "/transactions" },
+  { name: "Profile", path: "/profile" },
+];
+
+export const sidebarItemsAdmin = [
+  { name: "Dashboard", path: "/dashboard" },
+  { name: "Manage Users", path: "/users" },
+  { name: "Manage Accounts", path: "/accounts" },
+  { name: "Transactions", path: "/transactions" },
+  { name: "Reports", path: "/reports" },
+  { name: "Settings", path: "/settings" },
+];
