67fb85f13ce556c76e20819bb6b4599137f7a445
Shreejal Maharjan <shreejal27@gmail.com>
2025-08-24 23:40:32 +0545

    changed dashboard Icons for user

--- src/config/sidebarItems.tsx
+++ src/config/sidebarItems.tsx
@@ -1,18 +1,19 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import HistoryIcon from "@mui/icons-material/History";
 import PeopleIcon from "@mui/icons-material/People";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import SettingsIcon from "@mui/icons-material/Settings";
+import Saving from "@mui/icons-material/Savings";
+import Payment from "@mui/icons-material/Payments";
 
 export const sidebarItemsUser = [
   { name: "Dashboard", path: "/dashboard", icon: <DashboardIcon /> },
   { name: "Accounts", path: "/accounts", icon: <AccountBalanceIcon /> },
-  { name: "Deposit", path: "/deposit", icon: <AttachMoneyIcon /> },
-  { name: "Withdraw", path: "/withdraw", icon: <AttachMoneyIcon /> },
+  { name: "Deposit", path: "/deposit", icon: <Saving /> },
+  { name: "Withdraw", path: "/withdraw", icon: <Payment /> },
   { name: "Transfer", path: "/transfer", icon: <SwapHorizIcon /> },
   { name: "Transactions", path: "/transactions", icon: <HistoryIcon /> },
   { name: "Profile", path: "/profile", icon: <AccountCircleIcon /> },
