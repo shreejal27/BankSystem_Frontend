773732a25469703bee39fa30f27f28f5c8c8e317
Shreejal Maharjan <shreejal27@gmail.com>
2025-11-04 23:22:54 +0545

    made a command to get userAccountNumber

---
+++ src/queries/Transactions/TransactionsCommand.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { apiClientBe } from "../../api/Client/apiClientBe";
+import type { IGetUserAccountNumber } from "../../types/TransactionsDto";
+import { GetUserAccountNumberQueryKey } from "./TransactionsQueryKey";
+
+export const useGetUserAccountNumber = (id:string ) => {
+    return useQuery({
+    queryKey: [GetUserAccountNumberQueryKey, id],
+    queryFn: ()=> getUserAccountNumber(id),
+});
+};
+
+async function getUserAccountNumber(id: string): Promise<IGetUserAccountNumber> { 
+    const response = await apiClientBe.get<IGetUserAccountNumber>("api/Accounts/getAccountNumberById"+ id);
+    return response.data;
+}
---
+++ src/types/TransactionsDto.ts
@@ -0,0 +1,3 @@
+export interface IGetUserAccountNumber {
+    accountNumber: string;
+}
